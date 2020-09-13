3ec604490147070012d33c77e32ef5016190b828
Felipe Schein <felipe_str@hotmail.com>
2020-09-13 16:32:57 -0300

    Acesso ao Banco de Dados SQL e gravação em uma lista sem orientação a objeto.

---
+++ BD_SemOrientacaoObjeto/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BD_SemOrientacaoObjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Banco = new SqlConnection("Server=DESKTOP-IMST3LM\\SQLEXPRESS;Database=DBLICKOSKITEACHER; Integrated Security=True;");
+
+            Banco.Open();
+            string nome = "Felipe";
+            string sqlCommandTexto = "insert into PessoasNome(nome) values('" + nome + "')";
+            var comando = new SqlCommand(sqlCommandTexto, Banco);
+            comando.ExecuteNonQuery();
+
+            Banco.Close();
+        }
+    }
+}
